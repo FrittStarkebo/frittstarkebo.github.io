da3df9b68a5cc69a510e023871ec62c4544771c5
FrittStarkebo <46602272+FrittStarkebo@users.noreply.github.com>
2019-02-25 19:04:57 +0100

    Add files via upload

---
+++ kontakt.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+
+$(".v").mouseenter(function(){
+  $("#vinfo").show();
+
+
+});
+});
+
+$(".v").mouseleave(function(){
+  $("#vinfo").hide();
+
+
+});
+
+
+$(".m").mouseenter(function(){
+  $("#minfo").show();
+
+
+});
+
+$(".m").mouseleave(function(){
+  $("#minfo").hide();
+
+
+});
+
+$(".h").mouseenter(function(){
+  $("#hinfo").show();
+
+
+});
+
+$(".h").mouseleave(function(){
+  $("#hinfo").hide();
+
+
+});
